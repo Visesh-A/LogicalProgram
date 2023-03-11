4f1c0348766e33454ab0d71d713bf862b94e0960
“Visesh-A” <viseshajl@gmail.com>
2023-03-11 16:44:54 +0530

    [visesh]Add:Prime Number

---
+++ PrimeNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LogicalProgram
+{
+    public class PrimeNumber
+    {
+        public void IsPrime(int num)
+        {
+            int flag = 0;
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+                flag = 1;
+            }
+            if (flag == 0)
+                Console.WriteLine(num + " is not Prime");
+            else
+                Console.WriteLine(num + " is Prime");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace LogicalProgram
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Choose an option to execute \n1.Fibonacci Series \n2.Perfect Number \n3.Exit");
+                Console.WriteLine("Choose an option to execute \n1.Fibonacci Series \n2.Perfect Number \n3.Prime Number \n4.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -26,6 +26,12 @@ namespace LogicalProgram
                         perfectNumber.PerfectNum(num);
                         break;
                     case 3:
+                        Console.WriteLine("Enter number");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        PrimeNumber primeNumber = new PrimeNumber();
+                        primeNumber.IsPrime(n);
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
