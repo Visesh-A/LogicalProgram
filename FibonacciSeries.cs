81fd4151a6c88b1c2318bef2b68d54f29003aa36
“Visesh-A” <viseshajl@gmail.com>
2023-03-11 16:35:17 +0530

    [visesh]Add:Fibonacci Series

---
+++ FibonacciSeries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LogicalProgram
+{
+    public class FibonacciSeries
+    {
+        public void PrintSeries(int num)
+        {
+            int first = 0, second = 1, third = 0;
+            Console.WriteLine(first + "\n" + second);
+            for (int i = 2; i < num; i++)
+            {
+                third = first + second;
+                Console.WriteLine(third);
+                first = second;
+                second = third;
+            }
+        }
+    }
+}
+     
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LogicalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Programming");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose an option to execute \n1.Fibonacci Series \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter number");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.PrintSeries(number);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
