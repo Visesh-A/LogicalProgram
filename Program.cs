4f55b47e91f1bdf089ab239266c8997078d26160
“Visesh-A” <viseshajl@gmail.com>
2023-03-11 16:49:46 +0530

    [visesh]Add:Reverse Number

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace LogicalProgram
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Choose an option to execute \n1.Fibonacci Series \n2.Perfect Number \n3.Prime Number \n4.Exit");
+                Console.WriteLine("Choose an option to execute \n1.Fibonacci Series \n2.Perfect Number \n3.Prime Number \n4.Reverse a Number \n5.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -32,6 +32,12 @@ namespace LogicalProgram
                         primeNumber.IsPrime(n);
                         break;
                     case 4:
+                        Console.WriteLine("Enter number");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        ReverseNumber reverse = new ReverseNumber();
+                        reverse.Reverse(a);
+                        break;
+                    case 5:
                         flag = false;
                         break;
                 }
---
+++ ReverseNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LogicalProgram
+{
+    public class ReverseNumber
+    {
+        int rem = 0, res = 0;
+        public void Reverse(int n)
+        {
+            while (n > 0)
+            {
+                rem = n % 10;
+                res = res * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Reverse of Number is: " + res);
+        }
+    }
+}
